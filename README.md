5beeb87e1a98a076d1778b3bdf0ad295b8501a75
ivan <ivanpauno@gmail.com>
2017-07-17 19:05:12 -0300

    Starting code ... Added kivi garden matplotlib example. Updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # software
-Multi-platform software for data acquisition
+The computer-side app of smartbench project.
+A multi-platform software for data acquisition.
+
+Installing the framework, API, widgets, etc:
+
+Ubuntu ->
+	Kivy ->
+		sudo add-apt-repository ppa:kivy-team/kivy
+		sudo apt update
+		sudo apt install python-kivy
+		sudo apt install python3-kivy
+	pip  ->
+		sudo apt install python-pyp
+	Kivy Garden ->
+		pip install kivy garden
+	Kivy Garden graph ->
+		garden install graph
+		garden install --upgrade graph
+	Kivy Garden MatPlotLib ->
+		garden install matplotlib
+		garden install --upgrade matplotlib
+
+
+
+	
+
+
--- src/smartbench_app.py
+++ src/smartbench_app.py
@@ -1,8 +1,57 @@
+""" Smartbench app
+
+This app is the computer-side part of Smartbench project.
+You can control the scope, see the measured signal and posprocess it.
+
+Authors:
+            IP      Ivan Santiago Paunovic
+
+Version:
+            Date            Number          Name            Modified by     Comment
+            2017/07/17      0.1             SmBegins_mplf   IP              First approach. Using garden.matplotlib.
+
+ToDo:
+            Date            Suggested by    Activity                Description
+            2017/07/17      IP              Try garden.graph        Create another branch using garden.graph instead of garden.matplotlib.
+
+Releases:   In development ...
+"""
+import matplotlib
+matplotlib.use('module://kivy.garden.matplotlib.backend_kivy')
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvas,\
+                                                NavigationToolbar2Kivy
+
+
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
-class TestApp(App):
+from math import sin,pi
+
+
+x = range(0,500)
+y = [ sin(2*pi*i/500.) for i in range(0,500) ]
+
+fig, ax = plt.subplots()
+rects = ax.plot( x, y, 'r-' , label='y=sin(x)' )
+ax.set_ylabel('y')
+ax.set_title('A beautiful sinewave function')
+ax.set_xlabel('x')
+ax.legend( loc='upper right', shadow=True )
+
+canvas = fig.canvas
+
+class SmartbenchApp(App):
+    title = 'SmartbenchApp'
+
     def build(self):
-        return Button(text='Hello World')
+        fl = BoxLayout(orientation="vertical")
+        nav = NavigationToolbar2Kivy(canvas)
+        fl.add_widget(nav.actionbar)
+        fl.add_widget(canvas)
+        return fl
 
-TestApp().run()
+if __name__ == '__main__':
+    SmartbenchApp().run()
