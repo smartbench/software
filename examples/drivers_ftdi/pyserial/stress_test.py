fca9479f656b65a918ebc430dc4ad2a3306868aa
Andres Demski <andresdemski@gmail.com>
2017-09-05 02:51:02 -0300

    Better stress

--- examples/drivers_ftdi/pyserial/stress_test.py
+++ examples/drivers_ftdi/pyserial/stress_test.py
@@ -5,7 +5,12 @@
 # https://pythonhosted.org/pyserial/pyserial_api.html
 
 import serial
-ftdi = serial.Serial('/dev/ttyUSB1', baudrate=921600, timeout=2)
+import sys
+import time
+from statistics import mean
+
+
+ftdi = serial.Serial('/dev/ttyUSB1', baudrate=115200, timeout=2)
 ftdi.is_open
 
 N1 = 1000
@@ -15,16 +20,35 @@ N2 = 100000
 data_wr = range(N1)
 data_rd = []
 data = []
+errors = 0
+enviados = 0
+recibidos = 0
 
-for i in range(N2):
-    trama = bytes([i % 256])
-    ftdi.write(trama)
-    data_rd = list(ftdi.read(1))
-    data = data + data_rd
-    print( "({})\tin_waiting={}\tout_waiting={}\tdata_rd={}".format(i, ftdi.in_waiting, ftdi.out_waiting, data_rd) )
-    print( "Sent={}\tRead={}".format( list(trama) , list(data_rd) ) )
-    if(list(trama) != list(data_rd)):
-        break
+N= 30
+
+print("""Units:
+  Enviados: Bytes
+  Recibidos: Bytes
+  Errors: Bytes
+  Bps: Bytes por segundo
+""")
+while True:
+    t = []
+    trama = bytes([i%256 for i in range(10000) ])
+    for _ in range(N):
+        t1 = time.time()
+        enviados = enviados + ftdi.write(trama)
+        data_rd = ftdi.read(len(trama))
+        t2 = time.time()
+        t.append(t2-t1)
+        if trama != bytes(data_rd):
+            for n,c in enumerate(trama):
+                 if data_rd[n] != c:
+                        errors = errors + 1
+        recibidos = recibidos + len(data_rd)
+
+    t_mean = mean(t)
+    sys.stdout.write( "\r Enviados: {} Recibidos: {}  Errors: {} ({:.2}%)  Bps: {:.2}".format(enviados, recibidos, errors,errors/recibidos , len(trama)*2/(t_mean ) ))
 
 ftdi.close()
 
