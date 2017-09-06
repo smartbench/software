a68cb464457d059f9b5522019c8bad1f477cfa7e
Ariel Kukulanski <kuku2206@gmail.com>
2017-09-05 21:07:03 -0300

    test

--- examples/drivers_ftdi/pyserial/stress_test.py
+++ examples/drivers_ftdi/pyserial/stress_test.py
@@ -8,7 +8,7 @@ import serial
 ftdi = serial.Serial('/dev/ttyUSB1', baudrate=921600, timeout=2)
 ftdi.is_open
 
-N1 = 1000
+N1 = 100
 N2 = 100000
 #trama = bytes([1,2,3])
 
@@ -17,13 +17,18 @@ data_rd = []
 data = []
 
 for i in range(N2):
-    trama = bytes([i % 256])
+    data_rd = []
+    data = []
+    trama = bytes(i % 256 for i in range(i, i+N1))
     ftdi.write(trama)
-    data_rd = list(ftdi.read(1))
-    data = data + data_rd
-    print( "({})\tin_waiting={}\tout_waiting={}\tdata_rd={}".format(i, ftdi.in_waiting, ftdi.out_waiting, data_rd) )
-    print( "Sent={}\tRead={}".format( list(trama) , list(data_rd) ) )
+    while(len(list(data)) < len(list(trama))):
+        data_rd = list(ftdi.read(N1))
+        data = data + data_rd
+    #print( "({})\tin_waiting={}\tout_waiting={}\tdata_rd={}".format(i, ftdi.in_waiting, ftdi.out_waiting, data_rd) )
+    #print( "Sent={}\tRead={}".format( list(trama) , list(data_rd) ) )
+    if(0==(i%100)): print("\rN={}".format(i))
     if(list(trama) != list(data_rd)):
+        print( "\rSent={}\tRead={}".format( list(trama) , list(data_rd) ) )
         break
 
 ftdi.close()
