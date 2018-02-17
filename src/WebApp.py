4e5556ff7d54730cc6babd23ea540e4f5990cbef
Ariel Kukulanski <kuku2206@gmail.com>
2018-02-17 15:16:21 -0300

    two channels

--- src/WebApp.py
+++ src/WebApp.py
@@ -22,20 +22,30 @@ from bokeh.models import ColumnDataSource, Range1d
 from bokeh.models.layouts import Spacer
 from bokeh.models.widgets import Slider, TextInput
 from bokeh.plotting import figure
-
-# From here, handle the FTDI:
-from OscopeApi import *
-#from SmartbenchApp import *
+from bokeh.palettes import Viridis3
 
 from tornado import gen
 import tornado
 
+# From here, handle the FTDI:
+from OscopeApi import *
+from Configuration_Definitions import *
+#from SmartbenchApp import *
+
+
+_STATUS_STOPPED = 0
+_STATUS_RUNNING = 1
+
+_CHANNEL_ON     = 1
+_CHANNEL_OFF    = 0
 
 # Set up data
 N = 200
 x = np.linspace(0, 4*np.pi, N)
-y = np.sin(x)
-source = ColumnDataSource(data=dict(x=x, y=y))
+y_sin = np.sin(x)
+y_cos = np.cos(x)
+source_chA = ColumnDataSource(data=dict(x=x, y=y_sin))
+source_chB = ColumnDataSource(data=dict(x=x, y=y_cos))
 doc = curdoc()
 
 
@@ -44,7 +54,8 @@ plot = figure(plot_width= 100, plot_height= 100,sizing_mode='scale_both',title="
               tools="crosshair,pan,reset,save,wheel_zoom",
               x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
 
-plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
+plot.line('x', 'y', source=source_chA, line_width=3, line_alpha=0.6, color=Viridis3[0])
+plot.line('x', 'y', source=source_chB, line_width=3, line_alpha=0.6, color=Viridis3[1])
 
 
 # Set up widgets
@@ -73,9 +84,11 @@ def update_data(attrname, old, new):
 
     # Generate the new curve
     x = np.linspace(0, 4*np.pi, N)
-    y = a*np.sin(k*x + w) + b
+    y_sin = a*np.sin(k*x + w) + b
+    y_cos = a*np.cos(k*x + w) + b
 
-    source.data = dict(x=x, y=y)
+    source_chA.data = dict(x=x, y=y_sin)
+    source_chB.data = dict(x=x, y=y_cos)
 
 for w in [offset, amplitude, phase, freq]:
     w.on_change('value', update_data)
@@ -113,14 +126,27 @@ i = 0
 
 
 class SmartbenchApp():
-    global source,doc, plot
+    global source_chA, source_chB, doc, plot
 
     def __init__(self,**kwargs):
         self.smartbench = Smartbench()
         self.smartbench.setDefaultConfiguration()
+
+        # # ADC CLOCK SET TO 20MHz
+        # self.smartbench.chA.set_clk_divisor(Configuration_Definitions.Clock_Adc_Div_Sel[13])
+        # # AVERAGE DISABLED (1 SAMPLE)
+        self.smartbench.chA.set_nprom(Configuration_Definitions.Mov_Ave_Sel[13])
+        # # ADC CLOCK SET TO 20MHz
+        # self.smartbench.chB.set_clk_divisor(Configuration_Definitions.Clock_Adc_Div_Sel[13])
+        # # AVERAGE DISABLED (1 SAMPLE)
+        self.smartbench.chB.set_nprom(Configuration_Definitions.Mov_Ave_Sel[13])
+
+
         self.dataX = np.linspace(0, 4*np.pi, 200)
         self.dataY = np.cos(self.dataX)*np.cos(self.dataX)
-        source.data = dict(x=self.dataX, y=self.dataY)
+        self.dataY2 = np.sin(self.dataX)*np.cos(self.dataX)
+        source_chA.data = dict(x=self.dataX, y=self.dataY)
+        source_chB.data = dict(x=self.dataX, y=self.dataY2)
         print(">>><<<")
         doc.add_next_tick_callback(self.newFrameCallback)
 
@@ -163,18 +189,36 @@ class SmartbenchApp():
         print("> Request Stop")
         self.smartbench.request_stop()
 
-        # Then, requests the data.
-        print("> Request CHA")
-        self.smartbench.request_chA()
+        # If channel is ON, requests the data.
+        if(self.smartbench.get_chA_status() == _CHANNEL_ON):
+            print("> Request CHA")
+            self.smartbench.request_chA()
 
-        print("> Waiting...")
-        self.dataY = list(reversed(self.smartbench.receive_channel_data()))
-        self.dataX = range(0,len(self.dataY))
-        plot.x_range = Range1d(min(self.dataX), max(self.dataX))
-        plot.y_range = Range1d(min(self.dataY), max(self.dataY))
-        #self.mw.updatePlot( self.dataX, self.dataY )
-        #self.mw.plotTriggerPoint( self.smartbench.get_pretrigger()-1, self.smartbench.get_trigger_value() )
-        source.data = dict(x=self.dataX, y=self.dataY)
+            print("> Waiting...")
+            self.dataY_chA = list(reversed(self.smartbench.receive_channel_data()))
+            self.dataX_chA = range(0,len(self.dataY_chA))
+        else:
+            self.dataY_chA = []
+            self.dataX_chA = []
+
+        # If channel is ON, requests the data.
+        if(self.smartbench.get_chB_status() == _CHANNEL_ON):
+            # Then, requests the data.
+            print("> Request CHB")
+            self.smartbench.request_chB()
+
+            print("> Waiting...")
+            self.dataY_chB = list(reversed(self.smartbench.receive_channel_data()))
+            self.dataX_chB = range(0,len(self.dataY_chB))
+        else:
+            self.dataY_chB = []
+            self.dataX_chB = []
+
+        plot.x_range = Range1d(min(self.dataX_chA), max(self.dataX_chA))
+        plot.y_range = Range1d(min(self.dataY_chA), max(self.dataY_chA))
+
+        source_chA.data = dict(x=self.dataX_chA, y=self.dataY_chA)
+        source_chB.data = dict(x=self.dataX_chB, y=self.dataY_chB)
 
         if( self.smartbench.is_trigger_mode_auto() or self.smartbench.is_trigger_mode_normal() ):
             doc.add_next_tick_callback(self.newFrameCallback ) # Called as soon as possible
